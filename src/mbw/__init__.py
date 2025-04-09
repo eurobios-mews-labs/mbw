6093082a7cb8aa49647d7ec8066578165ef28e27
Emmanuel Cieren <129041266+ecieren@users.noreply.github.com>
2025-04-09 10:55:15 +0200

    Update __init__.py

--- src/mbw/__init__.py
+++ src/mbw/__init__.py
@@ -2,5 +2,5 @@ import os
 
 if "PYCHARM_HOSTED" in os.environ.keys() and os.environ["PYCHARM_HOSTED"] == "1":
     import matplotlib
-
-    matplotlib.use("TkAgg")
+    if matplotlib.get_backend() != matplotlib.get_backend():
+        matplotlib.use("TkAgg")
